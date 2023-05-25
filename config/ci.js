56aac94698d655f82c463ea5c2820eab000938e6
Spencer Cole <spencerlewiscole@gmail.com>
2023-05-25 18:56:56 -0400

    try with secrets in yaml and configs

--- config/ci.js
+++ config/ci.js
@@ -1,17 +1,7 @@
 module.exports = {
-  googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
+  googleClientID: GOOGLE_CLIENT_ID,
+  googleClientSecret: GOOGLE_CLIENT_SECRET,
+  mongoURI: CI_MONGO_URI,
+  cookieKey: COOKIE_KEY,
+  redisUrl: REDIS_URL,
 };
-
-
-// module.exports = {
-//     googleClientID: GOOGLE_CLIENT_ID,
-//     googleClientSecret: GOOGLE_CLIENT_SECRET,
-//     mongoURI: CI_MONGO_URI,
-//     cookieKey: COOKIE_KEY,
-//     redisUrl: REDIS_URL,
-//   };
