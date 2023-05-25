6ee507a4358268d8aee3816545cae9e0bbb01f31
Spencer Cole <spencerlewiscole@gmail.com>
2023-05-25 18:44:54 -0400

    ci temp test

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,16 @@
 module.exports = {
-    googleClientID: GOOGLE_CLIENT_ID,
-    googleClientSecret: GOOGLE_CLIENT_SECRET,
-    mongoURI: CI_MONGO_URI,
-    cookieKey: COOKIE_KEY,
-    redisUrl: REDIS_URL,
-  };
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
+};
+
+// module.exports = {
+//     googleClientID: GOOGLE_CLIENT_ID,
+//     googleClientSecret: GOOGLE_CLIENT_SECRET,
+//     mongoURI: CI_MONGO_URI,
+//     cookieKey: COOKIE_KEY,
+//     redisUrl: REDIS_URL,
+//   };
--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,12 @@
+// module.exports = {
+//   googleClientID:
+//     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+//   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+//   mongoURI:
+//     "mongodb+srv://colespen:xE81JGkpyYy4Iofv@cluster0.qpby1ts.mongodb.net/advnode?retryWrites=true&w=majority",
+//   cookieKey: "123123123",
+//   redisUrl: "redis://127.0.0.1:6379",
+// };
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
