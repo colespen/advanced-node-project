7b33f862cfd7c5fbcda8d10c63e7609d0d851afb
Spencer Cole <spencerlewiscole@gmail.com>
2023-05-26 16:17:55 -0400

    alt tag update

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -16,7 +16,7 @@ class BlogShow extends Component {
             "https://node-blog-course.s3.us-east-2.amazonaws.com/" +
             this.props.blog.imageUrl
           }
-          alt="blog attached picture"
+          alt="user uploaded file for blog"
         />
       );
     }
